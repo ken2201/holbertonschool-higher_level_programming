14176fd0c1cfdb58109a8f64e795cf2b1dff5dcc
ken2201 <ndayishimiyeken22@gmail.com>
2024-02-01 17:31:18 -0800

    more data and structure

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    if my_list is not None:
-        return([x if x != search else replace for x in my_list])
-    return None
+def uniq_add(my_list=[]):
+    total = 0
+    for i in set(my_list):
+        total += i
+    return(total)
