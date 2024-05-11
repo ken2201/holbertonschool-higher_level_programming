d5318d9b9508f37842e82c473fdc4a11324e0e3f
ken2201 <ndayishimiyeken22@gmail.com>
2024-05-11 15:39:00 +0200

    Fix bug in login functionality

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number < 0:
+    last_digit = number % -10
+else:
+    last_digit = number % 10
+
+    string = "Last digit of {number} is {last}"
+
+    if last_digit > 5:
+        print(f"{string} and is greater than 5")
+    elif last_digit == 0:
+        print(f"{string} and is 0")
+    else:
+        print(f"{string} and is less than 6 and not 0")
