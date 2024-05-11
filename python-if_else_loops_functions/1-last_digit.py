cf4ddd2160fb66f25cf72db5a0981d94e8c77ff4
ken2201 <ndayishimiyeken22@gmail.com>
2024-05-11 15:43:03 +0200

    Refactor last digit calculation and print statement

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,16 +2,16 @@
 import random
 number = random.randint(-10000, 10000)
 
-if number < 0:
-    last_digit = number % -10
+if (number < 0):
+    last = number % -10
 else:
-    last_digit = number % 10
+    last = number % 10
 
-    string = "Last digit of {number} is {last}"
+string = f"Last digit of {number} is {last}"
 
-    if last_digit > 5:
-        print(f"{string} and is greater than 5")
-    elif last_digit == 0:
-        print(f"{string} and is 0")
-    else:
-        print(f"{string} and is less than 6 and not 0")
+if (last > 5):
+    print(f"{string} and is greater than 5")
+elif (last == 0):
+    print(f"{string} and is 0")
+else:
+    print(f"{string} and is less than 6 and not 0")
