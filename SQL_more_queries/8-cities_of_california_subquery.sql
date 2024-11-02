40aaa02e3d0d70c0b2694c18ce0f7743d5c4885b
ken2201 <ndayishimiyeken22@gmail.com>
2024-11-02 23:21:25 +0100

    more data but last tetst

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+    LIMIT 1
+)
+ORDER BY cities.id ASC;
